0ae7056445548701146ffd626f34bc100a78a54e
Eszter Toth <testimo@Eszters-MBP.fritz.box>
2023-03-05 13:58:27 +0100

    v2 nav works

--- App.js
+++ App.js
@@ -2,26 +2,38 @@
 import 'react-native-url-polyfill/auto'
 const supabaseUrl = 'https://qgvehrcffejjqaprawiz.supabase.co'
 import { supabase } from './supabase/supabase';
-// import { createClient } from '@supabase/supabase-js'
 
 
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React, { Component,useLayoutEffect }  from 'react';
-import { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState,Component,useLayoutEffect }  from 'react';
+
+
 import { useFonts, AmaticSC_400Regular } from '@expo-google-fonts/amatic-sc';
 import * as Font from 'expo-font';
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Travel from "./screens/Travel";
-import FarmMap from "./screens/FarmMap";
 import AddNewPlace from './screens/AddNewPlace';
-import SideMenu from './screens/SideMenu';
-import FloatButton from './screens/FloatButton';
+// import HomeScreen from './screens/HomeScreen';
 
+
+const Drawer = createDrawerNavigator();
+
+class FloatButton extends Component {
+  handlePress = () => {
+    this.props.navigation.openDrawer();
+  };
+  render() {
+    return (
+      <View className="flex-row absolute top-0 left-0 px-6 mt-8 items-center space-x-2">
+        <TouchableOpacity title="Open Drawer" onPress={this.handlePress} className="flex-none w-16 h-16 bg-haztaji rounded-full justify-center">
+          <Image style={{width: 44, height: 44, margin:10}} source={require('./assets/haztaji-logo.png')} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -67,7 +79,7 @@ export default function App() {
 
 
 
-  const Stack = createStackNavigator();
+  // const Stack = createStackNavigator();
   return (
   //   <NavigationContainer>
   //   <Stack.Navigator>
@@ -77,14 +89,39 @@ export default function App() {
   //   </Stack.Navigator>
   // </NavigationContainer>
       <NavigationContainer>
-      <Drawer.Navigator
-        // drawerContent={(props) => <FloatButton {...props} />}
-        initialRouteName="HomeScreen"
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Új hely hozzáadása" component={AddNewPlace} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+          <Drawer.Navigator
+            // drawerContent={(props) => <FloatButton {...props} />}
+            initialRouteName="HomeScreen"
+          >
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Add New Place" component={AddNewPlace} />
+          </Drawer.Navigator>
+      </NavigationContainer>
+  );
+}
+
+function HomeScreen(props) {
+  const navigation = useNavigation();
+
+   useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, [navigation]);
+
+  return (
+    <SafeAreaView className="flex-1 relative">
+        <View className="flex-1 relative">
+            <MapView 
+            className="flex-1 relative" />
+            <FloatButton navigation={navigation} />
+            <View className="flex-row absolute top-0 left-0 px-6 mt-8 items-center space-x-2">
+                <View className="flex-auto justify-center items-center">
+                <Image style={{width:75, height:40}} source={require('./assets/haztaji-text-logo.png')} />
+                </View>
+            </View>
+        </View>
+    </SafeAreaView>
   );
 }
 
