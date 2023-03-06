c64d2521a19a6f6f29bde7d575706b0ffadbc1f3
Eszter Toth <testimo@Eszters-MacBook-Pro.local>
2023-03-06 12:37:50 +0100

    homescreen working with dropdownselect, database working, homescreen user added markers missing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import 'react-native-gesture-handler';
-import { View, SafeAreaView, Image } from 'react-native'
-import React from 'react'
+import { View, SafeAreaView, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import SelectDropdown from 'react-native-select-dropdown';
 import MapView from 'react-native-maps';
 import FloatButton from './FloatButton';
 import FarmMap from './FarmMap';
+
+const products = ["Eggs", "Honey", "Tomato", "Pepper", "Apples","Strawberry","Melon","Chicken","Mushroom"];
 
 export default function HomeScreen({ navigation }) {
   return (
@@ -15,6 +18,50 @@ export default function HomeScreen({ navigation }) {
                 <Image style={{width:75, height:40}} source={require('../assets/haztaji-text-logo.png')} />
             </View>
         </View>
+        <View style={styles.dropdownContainer}>
+
+              <SelectDropdown
+                          data={products}
+                          onSelect={(selectedItem, index) => {
+                            console.log(selectedItem, index)
+                          }}
+                          defaultButtonText={"What would you like to buy?"}
+                          dropdownOverlayColor={"transparent"}
+                          search={true}
+                          buttonStyle={{width: '95%', border:'solid', borderWidth: 1, borderColor: 'black', borderRadius: 10}}
+                          dropdownStyle={{width: '100%', height: '100%'}}
+                          rowTextStyle={{color: 'black', fontSize: 20,}}
+                          // rowStyle={{width: '100%', height: '100%', backgroundColor: 'white',}}
+                          // searchInputStyle={{color: 'black', fontSize: 20,width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, backgroundColor: 'white',}}
+                          searchPlaceHolderText={"Search products..."}
+                          buttonTextAfterSelection={(selectedItem, index) => {
+                            // text represented after item is selected
+                            // if data array is an array of objects then return selectedItem.property to render after item is selected
+                            return selectedItem
+                          }}
+                          rowTextForSelection={(item, index) => {
+                            // text represented for each item in dropdown
+                            // if data array is an array of objects then return item.property to repreeggsent item in dropdown
+                            return item
+                          }}
+                  />
+          </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dropdownContainer: {
+    position: 'absolute',
+    bottom: 10,
+    width: '100%',
+    left: 10,
+    right: 10,
+    zIndex: 1,
+  },
+});
